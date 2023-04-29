258391aea480f0c98da9e0fc7c03c5973d63f87d
ZAHPOT <freetokdevelop@gmail.com>
2023-04-29 15:57:37 +0300

    Fixed movement

--- Assets/_Scripts/TaskManager.cs
+++ Assets/_Scripts/TaskManager.cs
@@ -96,6 +96,11 @@ namespace _Scripts
         {
             Invoke(nameof(StartNewTask), timeToWaitBeforeStartNewTask);
         }
-    
+
+        public void RemoveGrabEvent()
+        {
+            
+            grab.selectEntered.RemoveAllListeners();
+        }
     }
 }
